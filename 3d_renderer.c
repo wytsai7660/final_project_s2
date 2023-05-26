8b373b7dcdbd0e6295943b6b2e8b06db3c408a8a
AlanLiang-314 <61128794+AlanLiang-314@users.noreply.github.com>
2023-05-26 09:26:05 +0000

    update 3d_renderer.c

--- 3d_renderer.c
+++ 3d_renderer.c
@@ -1,9 +1,10 @@
 #include "header.h"
 
+
 bool new_tan(float x, float y, double angle){
-    double normAngle = fmod(angle, 2 * M_PI);
-    if(normAngle < 0) normAngle += 2 * M_PI; 
-    if (normAngle > M_PI/2 && normAngle <= 3*M_PI/2)
+    double normAngle = fmod(angle, 2 * PI);
+    if(normAngle < 0) normAngle += 2 * PI; 
+    if (normAngle > PI/2 && normAngle <= 3*PI/2)
     {
         return y >= tan(normAngle)*x;
     }else
@@ -13,12 +14,12 @@ bool new_tan(float x, float y, double angle){
 }
 
 void scan(int row, int col, double pov){
-    int degree = (int)M_PI * 400;
+    int degree = (int)PI * 400;
     for(int k = 0; k < degree;k++)
     {
         for(int i=-row/2;i<row/2;i++){
             for(int j=-col/2;j<col/2;j++){
-                if(new_tan(i, j, k/100.0 - pov/2.0 - M_PI/2) && new_tan(i, j, k/100.0 + pov/2.0 - M_PI/2)){
+                if(new_tan(i, j, k/100.0 - pov - PI/2) && new_tan(i, j, k/100.0 - PI/2)){
                     printf("0 ");
                 }else{
                     printf("  ");
@@ -26,7 +27,7 @@ void scan(int row, int col, double pov){
             }
             printf("\n");
         }
-        if (k == 0) usleep(1000*1000);
+        if (k == 0) usleep(5000*1000);
         usleep(30*1000);
         printf("\e[1;1H\e[2J");
 
@@ -34,6 +35,27 @@ void scan(int row, int col, double pov){
 
 }
 
+void printFOV(int row, int col, double pov){
+
+    for(int i=-row/2;i<row/2+1;i++){
+        for(int j=-col/2;j<col/2+1;j++){
+            if(new_tan(i, j,  - pov) && new_tan(i, j,  0)){
+                printf("0 ");
+            }else{
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    usleep(1000*1000);
+}
+
 int main(){
-    scan(30, 50, M_PI/3);
+    // scan(30, 50, fov);
+    printFOV(30, 50, PI/6);
+    printFOV(30, 50, PI/5);
+    printFOV(30, 50, PI/4);
+    printFOV(30, 50, PI/3);
+    printFOV(30, 50, PI/2);
+    printFOV(30, 50, 2*PI/3);
 }
