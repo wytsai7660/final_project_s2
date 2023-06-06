0989d88b91e9252637561d230e763fb13720f67e
AlanLiang-314 <61128794+AlanLiang-314@users.noreply.github.com>
2023-06-06 09:08:27 +0000

    update drawMiniMap

--- small_map.c
+++ small_map.c
@@ -1,21 +1,10 @@
 #include "map.c"
 #include "header.h"
-#include <termios.h>
-
-#define RED   "\x1B[31m"
-#define GRN   "\x1B[32m"
-#define YEL   "\x1B[33m"
-#define BLU   "\x1B[34m"
-#define MAG   "\x1B[35m"
-#define CYN   "\x1B[36m"
-#define WHT   "\x1B[37m"
-#define ATI   "\x1B[17m"
-#define RESET "\x1B[0m"
-#define DEBUG
 
 
-void drawMiniMap(Map *map, IntPair *playerPos, int smallMapSize, int watchTowerCnt){
-    printf("player pos %d, %d\n", playerPos->first, playerPos->second);
+void drawMiniMap(Map *map, IntPair *playerPos, int smallMapSize, int watchTowerCnt, int y, int x){
+    // printf("player pos %d, %d\n", playerPos->first, playerPos->second);
+    printf("\e[%d;%dH", y, x);
 
     int xcenter = max(smallMapSize, min(playerPos->first, map->row - 1 - smallMapSize));
     int ycenter = max(smallMapSize, min(playerPos->second, map->col - 1 - smallMapSize));
@@ -55,6 +44,9 @@ void drawMiniMap(Map *map, IntPair *playerPos, int smallMapSize, int watchTowerC
                         case '7':
                             printf(MAG "%c " RESET, 'W');
                             break;
+                        case '8':
+                            printf(MAG "%c " RESET, 'I');
+                            break;
                         default:
                             printf("%c ", ' ');
                             break;                        
@@ -66,20 +58,15 @@ void drawMiniMap(Map *map, IntPair *playerPos, int smallMapSize, int watchTowerC
             }
         }
         printf("\n");
+        printf("\e[%d;%dH", ++y, x);
     }
 }
 
-void fight() {
-    printf("fight\n");
-}
 
-void gainItem() {
-    printf("gainItem\n");
-}
-
-void playerEvent(Map *m, IntPair *playerPos, PlayerData *p){
+void playerEvent(Map *m, IntPair *playerPos, PlayerData *p, Game *game){
     char ch = m->data[playerPos->first][playerPos->second];
     m->data[playerPos->first][playerPos->second] = '9';
+    int item;
     switch (ch)
     {
     case '0':
@@ -101,21 +88,21 @@ void playerEvent(Map *m, IntPair *playerPos, PlayerData *p){
         p->def = max(p->def-1, 0);
         break;
     case '6':
-        fight();
+        // game->status = 3;
         break;
     case '7':
         p->watchTowerCnt = 10;
         break;
     case '8':
-        gainItem();
+        item = sample(items_ratio, sizeof(items_ratio)/sizeof(float));
+        p->backpack[item]++;
         break;
     default:
         break;
     }
 }
 
-
-int main(){
+void mapLoop() {
     struct termios old_attr, new_attr;
     tcgetattr(STDIN_FILENO, &old_attr);
     new_attr = old_attr;
@@ -126,57 +113,67 @@ int main(){
     srand((unsigned)time(NULL));
     Map *map = new_Map(17, 87);
     PlayerData *player = new_PlayerData();
+    Game *game = new_Game();
     int smallMapSize = 5;
-    IntPair playerPos;
+    game->status = 2;
+
     char ch;
 
     gen_maze(map);
     for (int i = 0; i < map->row; i++) {
         for (int j = 0; j < map->col; j++) {
             if(map->data[i][j] == 'P'){
-                playerPos = make_IntPair(i, j);
+                player->pos = make_IntPair(i, j);
             }
         } 
     }
-    int dir;
 
     // game loop
-    while(read(STDIN_FILENO, &ch, 1) != '0')
+    while(game->status == 2 && read(STDIN_FILENO, &ch, 1))
     {
         printf("\e[1;1H\e[2J");
         printf("\e[?25l");
         printf("hp %d atk %d def %d\n", player->hp, player->atk, player->def);
         printf("watchTowerCnt %d\n", player->watchTowerCnt);
-        drawMiniMap(map, &playerPos, smallMapSize, player->watchTowerCnt);
+        drawMiniMap(map, &player->pos, smallMapSize, player->watchTowerCnt, 1, 1);
+
         if(ch == 'w')
         {
-            dir = 2;
+            player->dir = 2;
         }
         else if(ch == 'a')
         {
-            dir = 1;
+            player->dir = 1;
         }
         else if(ch == 's')
         {
-            dir = 0;
+            player->dir = 0;
         }
         else if(ch == 'd')
         {
-            dir = 3;
+            player->dir = 3;
         }else{
             continue;
         }
-        
-        // clear affect
-        player->watchTowerCnt -= player->watchTowerCnt ? 1 : 0;
 
-        if (!(map->data[playerPos.first + direction[dir][0]][playerPos.second + direction[dir][1]] == '@'))
+        if (!(map->data[player->pos.first + direction[player->dir][0]][player->pos.second + direction[player->dir][1]] == '@'))
         {
-            playerPos.first += direction[dir][0];
-            playerPos.second += direction[dir][1];
-            playerEvent(map, &playerPos, player);
+            player->watchTowerCnt -= player->watchTowerCnt ? 1 : 0;
+            player->pos.first += direction[player->dir][0];
+            player->pos.second += direction[player->dir][1];
+            // IntPairList_push(game->playerPath, (IntPair){player->pos.first, player->pos.second});
+            playerEvent(map, &player->pos, player, game);
         }
-        usleep(30*1000);
+        // generate new Map events
+        // if list > mapsize**2 / 6
+        // int newMapEvent = sample(events_ratio, sizeof(events_ratio)/sizeof(float));
+        // map->data[][] = newMapEvent + '0';
+        // usleep(30*1000);
     }
     tcsetattr(STDIN_FILENO, TCSANOW, &old_attr);
 }
+
+
+// int main(){
+    
+// }
